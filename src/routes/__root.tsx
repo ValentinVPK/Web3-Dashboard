0d8d4c5eec299850eda7ca9d5db95dfe81e0f1b3
ValentinVPK <valentinvpk@abv.bg>
2026-03-26 16:09:45 +0200

    fix: small import fix

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,6 @@
-import Navigation from "@/components/layout/Navigation";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import Navigation from "@/components/layout/Navigation";
 
 const RootLayout = () => (
   <>
